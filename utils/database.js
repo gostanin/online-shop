0a3e1061d399a1a8cb033da8b6cd23c293f5c93e
Gregory Ostanin <ostingreg@gmail.com>
2020-09-28 11:41:55 -0700

    Update database connection

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'nodejs',
-    database: 'nodejspassword'
+    database: 'node-complete',
+    password: 'nodejspassword'
 });
 
 module.exports = pool.promise();
