114f8f2b415f3cf111eef415c12501eb14b3e879
Gregory Ostanin <ostingreg@gmail.com>
2020-09-29 11:03:02 -0700

    Update product model logic to utilize sequelize

--- models/product.js
+++ models/product.js
@@ -1,30 +1,27 @@
-const db = require('../utils/database');
+const Sequelize = require('sequelize')
 
-const Cart = require('./cart');
+const sequelize = requrie('../utils/database.js')
 
-module.exports = class Product {
-    constructor(id, title, price, description, imageUrl) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.description = description;
-    }
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    } 
+})
 
-    save() {
-        return db.execute('insert into products(title, price, description, imageUrl) values(?, ?, ?, ?)', 
-                            [this.title, this.price, this.description, this.imageUrl]);
-    }
-
-    static fetchAll() {
-        return db.execute('select * from products');
-    }
-
-    static delete(id) {
-        return db.execute(`delete from products where id = ?`, [id])
-    }
-
-    static findById(id) {
-        return db.execute(`select * from products where id = ?`, [id])
-    }
-};
+module.exports = Product;
