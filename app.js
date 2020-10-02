459db1090fe40e35fca369ae11d836a8ff635e99
Gregory Ostanin <ostingreg@gmail.com>
2020-10-01 18:46:09 -0700

    Add database association

--- app.js
+++ app.js
@@ -10,16 +10,27 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/shop')
 const errorController = require('./controllers/error');
+const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(error => console.log(error));
+});
 //request, response, next(function on a list)
 //order of a middle ware is important
 
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
-
-
 
 app.use(errorController.get404)
 
@@ -27,4 +38,20 @@ app.use(errorController.get404)
 
 // server.listen(8080);
 
-app.listen(3000);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize.sync()
+    .then(() => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({name: 'Admin', email: 'test@test.com'});
+        }
+        return user; //Promise.resolve(user) not need to use becaues then block automatically wraps return statmenet into Promise;
+    })
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(error => console.log(error));
