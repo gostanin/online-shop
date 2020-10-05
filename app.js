e10baa6521cfd44a7b0b5f87dcf0609666924876
Gregory Ostanin <ostingreg@gmail.com>
2020-10-05 11:55:59 -0700

    Update application to connect to mongodb

--- app.js
+++ app.js
@@ -8,65 +8,35 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/shop')
-const errorController = require('./controllers/error');
-const sequelize = require('./utils/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');;
-const Order = require('./models/order');;
-const OrderItem = require('./models/orderItems');
+const userRoutes = require('./routes/shop');
+// const errorController = require('./controllers/error');
+const mongoConnect = require('./utils/database').mongoConnect;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(error => console.log(error));
-});
+// app.use((req, res, next) => {
+//     User.findByPk(1)
+//         .then(user => {
+//             req.user = user;
+//             next();
+//         })
+//         .catch(error => console.log(error));
+// });
 //request, response, next(function on a list)
 //order of a middle ware is important
 
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
 
-app.use(errorController.get404)
+// app.use(errorController.get404)
 
 // const server = http.createServer(app); //ctrl+shift+space
 
 // server.listen(8080);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem })
+mongoConnect(() => {
+    app.listen(3000);
+})
 
-// {force:true}
-sequelize.sync()
-    .then(() => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            return User.create({ name: 'Admin', email: 'test@test.com' });
-        }
-        return user; //Promise.resolve(user) not need to use becaues then block automatically wraps return statmenet into Promise;
-    })
-    .then(user => {
-        return user.createCart();
-    })
-    .then(() => {
-        app.listen(3000);
-    })
-    .catch(error => console.log(error));
