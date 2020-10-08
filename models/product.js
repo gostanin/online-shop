229a888c68aeabb3cb35d216ba8e246e03b46c32
Gregory Ostanin <ostingreg@gmail.com>
2020-10-07 20:41:50 -0700

    Update models to use mongodb

--- models/product.js
+++ models/product.js
@@ -1,27 +1,67 @@
-const Sequelize = require('sequelize')
+const mongodb = require("mongodb");
+const getDb = require("../utils/database.js").getDb;
 
-const sequelize = require('../utils/database.js')
+class Product {
+    constructor(title, price, description, imageUrl, id, userId) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this._id = id ? new mongodb.ObjectId(id) : null;
+        this.userId = userId
+    }
 
-const Product = sequelize.define('product', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    title: Sequelize.STRING,
-    price: {
-        type: Sequelize.DOUBLE,
-        allowNull: false
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.STRING,
-        allowNull: false
-    } 
-});
+    save() {
+        const db = getDb();
+        let result;
+        if (this._id) {
+            result = db
+                .collection("products")
+                .updateOne({ _id: this._id }, { $set: this });
+        } else {
+            result = db.collection("products").insertOne(this);
+        }
+        return result
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((error) => console.log(error));
+    }
+
+    static fetchAll() {
+        const db = getDb();
+        return db
+            .collection("products")
+            .find()
+            .toArray()
+            .then((products) => {
+                return products;
+            })
+            .catch((error) => console.log(error));
+    }
+
+    static findById(id) {
+        const db = getDb();
+        return db
+            .collection("products")
+            .find({ _id: new mongodb.ObjectId(id) })
+            .next()
+            .then((product) => {
+                return product;
+            })
+            .catch((error) => console.log(error));
+    }
+
+    static deleteById(id) {
+        const db = getDb();
+        return db
+            .collection("products")
+            .deleteOne({ _id: new mongodb.ObjectId(id) })
+            .then((result) => {
+                console.log('deleted');
+            })
+            .catch((error) => console.log(error));
+    }
+}
 
 module.exports = Product;
