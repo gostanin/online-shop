b51a6304da671ea05f4b664d9d86959f91830a8c
Gregory Ostanin <ostingreg@gmail.com>
2020-10-07 20:41:11 -0700

    Update app to use mongodb

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express(); // valid request listener
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/shop');
-// const errorController = require('./controllers/error');
-const mongoConnect = require('./utils/database').mongoConnect;
-
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/shop");
+const errorController = require('./controllers/error');
+const mongoConnect = require("./utils/database").mongoConnect;
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//     User.findByPk(1)
-//         .then(user => {
-//             req.user = user;
-//             next();
-//         })
-//         .catch(error => console.log(error));
-// });
+app.use((req, res, next) => {
+    User.findById("5f7cc6b6c2611fb87de145ea")
+        .then(user => {
+            req.user = new User(user.name, user.email, user.cart, user._id);
+            next();
+        })
+        .catch(error => console.log(error));
+});
 //request, response, next(function on a list)
 //order of a middle ware is important
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(userRoutes);
 
-// app.use(errorController.get404)
+app.use(errorController.get404)
 
 // const server = http.createServer(app); //ctrl+shift+space
 
@@ -38,5 +38,4 @@ app.use(userRoutes);
 
 mongoConnect(() => {
     app.listen(3000);
-})
-
+});
