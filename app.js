e82131a2afa481aeb2820a1c2893b0e11610ba83
Gregory Ostanin <ostingreg@gmail.com>
2020-10-12 08:39:45 -0700

    Add session

--- app.js
+++ app.js
@@ -2,24 +2,43 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MondoDBStore = require("connect-mongodb-session")(session);
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express(); // valid request listener
+const store = new MondoDBStore({
+    uri: `mongodb+srv://***REMOVED***:${process.env.DB_PSW}@cluster0.dpwzp.mongodb.net/***REMOVED***`,
+    collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+    session({
+        secret: "my secret a long string value",
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+        // unset: "destroy",
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById("5f7f8a00ad94cf18f0297991")
+    if (!req.session.user) {
+        return next();
+    }s
+    User.findById(req.session.user._id)
         .then((user) => {
             req.user = user;
             next();
@@ -31,6 +50,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(userRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -40,7 +60,8 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        `mongodb+srv://***REMOVED***:${process.env.DB_PSW}@cluster0.dpwzp.mongodb.net/***REMOVED***`
+        `mongodb+srv://***REMOVED***:${process.env.DB_PSW}@cluster0.dpwzp.mongodb.net/***REMOVED***`,
+        { useUnifiedTopology: true, useNewUrlParser: true }
     )
     .then((result) => {
         User.findOne().then((user) => {
@@ -49,7 +70,7 @@ mongoose
                     name: "Admin",
                     email: "test@test.com",
                 });
-            user.save();
+                user.save();
             }
         });
         app.listen(3000);
