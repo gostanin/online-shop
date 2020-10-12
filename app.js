985c28427f69f1f777ec8a96e76db575485125d4
Gregory Ostanin <ostingreg@gmail.com>
2020-10-12 09:00:17 -0700

    Fix typo

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
 app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
-    }s
+    }
     User.findById(req.session.user._id)
         .then((user) => {
             req.user = user;
